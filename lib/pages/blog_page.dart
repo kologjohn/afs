faccfc04fbf8fc0e289b45cc378b271fe9138dbc
kologjohn <kologjohndok@gmail.com>
2024-10-13 23:53:45 +0000

    Merge remote-tracking branch 'origin/main'

--- lib/pages/blog_page.dart
+++ lib/pages/blog_page.dart
@@ -1,6 +1,7 @@
 import 'package:africanstraw/blog/blog_post.dart';
 import 'package:flutter/material.dart';
 import '../constanst.dart';
+import '../widgets/post_blog.dart';
 import '../widgets/recent_post.dart';
 
 class BlogPage extends StatelessWidget {
@@ -27,6 +28,15 @@ class BlogPage extends StatelessWidget {
       crossAxisCount = 1;
     }
 
+    bool isVisible () {
+      double screenWidth = MediaQuery.of(context).size.width;
+      return screenWidth > 1400;
+    }
+    bool isNotVisible () {
+      double screenWidth = MediaQuery.of(context).size.width;
+      return screenWidth < 1200;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Our Blog"),
@@ -39,72 +49,75 @@ class BlogPage extends StatelessWidget {
             : const EdgeInsets.only(left: 100.0, right: 100.0, top: 50),
         child: Row(
           children: [
-            Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            constraints: const BoxConstraints(maxWidth: 400, minWidth: 400), // Limit width
-                            child: Row(
-                              children: [
-                                // Search Input
-                                Flexible(
-                                  flex: 3,
-                                  child: TextField(
-                                    controller: searchController,
-                                    decoration: InputDecoration(
-                                      hintText: 'Search...',
-                                      hintStyle: TextStyle(
-                                        color: Colors.grey[500],
-                                        fontSize: 16,
-                                      ),
-                                      fillColor: Colors.white,
-                                      filled: true,
-                                      suffixIcon: Icon(Icons.search, color: Colors.blue), // Search Icon
-                                      contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15), // Padding inside TextField
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(30), // Rounded corners
-                                        borderSide: BorderSide.none, // No visible border
-                                      ),
-                                      enabledBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(30),
-                                        borderSide: const BorderSide(
-                                            color: Colors.blue,
-                                            width: 1
+            Visibility(
+              visible: isVisible(),
+              child: Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              constraints: const BoxConstraints(maxWidth: 400, minWidth: 400), // Limit width
+                              child: Row(
+                                children: [
+                                  // Search Input
+                                  Flexible(
+                                    flex: 3,
+                                    child: TextField(
+                                      controller: searchController,
+                                      decoration: InputDecoration(
+                                        hintText: 'Search...',
+                                        hintStyle: TextStyle(
+                                          color: Colors.grey[500],
+                                          fontSize: 16,
+                                        ),
+                                        fillColor: Colors.white,
+                                        filled: true,
+                                        suffixIcon: const Icon(Icons.search, color: Colors.blue), // Search Icon
+                                        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15), // Padding inside TextField
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(30), // Rounded corners
+                                          borderSide: BorderSide.none, // No visible border
+                                        ),
+                                        enabledBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(30),
+                                          borderSide: const BorderSide(
+                                              color: Colors.blue,
+                                              width: 1
+                                          ),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(30),
+                                          borderSide: BorderSide.none,
                                         ),
                                       ),
-                                      focusedBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(30),
-                                        borderSide: BorderSide.none,
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 16,
                                       ),
                                     ),
-                                    style: const TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(height: 40),
-                    const Text("RECENT NEWS", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                    const SizedBox(height: 20),
-                    const RecentPost(title: 'TRENDING FASHION', date: 'October 02, 2024', imageUrl: 'assets/images/A8A0463.jpg',),
-                    const SizedBox(height: 10),
-                    const RecentPost(title: 'BOLGA ROUND BASKETS', date: 'October 01, 2024', imageUrl: 'assets/images/A8A0487.jpg',),
-                    const SizedBox(height: 10),
-                    const RecentPost(title: 'INTERIOR DECOR', date: 'September 28, 2024', imageUrl: 'assets/images/A8A0530.jpg',),
-                    const SizedBox(height: 10),
-                    const RecentPost(title: 'ARTISANS', date: 'September 20, 2024', imageUrl: 'assets/images/slide.jpg',),
-                  ],
-                )
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 40),
+                      const Text("RECENT NEWS", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                      const SizedBox(height: 20),
+                      const RecentPost(title: 'TRENDING FASHION', date: 'October 02, 2024', imageUrl: 'assets/images/A8A0463.jpg',),
+                      const SizedBox(height: 10),
+                      const RecentPost(title: 'BOLGA ROUND BASKETS', date: 'October 01, 2024', imageUrl: 'assets/images/A8A0487.jpg',),
+                      const SizedBox(height: 10),
+                      const RecentPost(title: 'INTERIOR DECOR', date: 'September 28, 2024', imageUrl: 'assets/images/A8A0530.jpg',),
+                      const SizedBox(height: 10),
+                      const RecentPost(title: 'ARTISANS', date: 'September 20, 2024', imageUrl: 'assets/images/slide.jpg',),
+                    ],
+                  )
+              ),
             ),
             Expanded(
               flex: 3,
@@ -114,38 +127,6 @@ class BlogPage extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-
-class PostWidget extends StatelessWidget {
-  const PostWidget({
-    super.key,
-    required this.crossAxisCount,
-  });
-
-  final int crossAxisCount;
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 8,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        mainAxisSpacing: 16,
-        crossAxisSpacing: 16,
-        childAspectRatio: 0.7, // Adjust based on your design
-        crossAxisCount: crossAxisCount,
-      ),
-      itemBuilder: (context, index) {
-        return BlogPost(
-          title: 'TRENDING FASHIONS',
-          mainText: Companydata.blogDescription,
-          date: 'August 18, 2024',
-          comments: '69',
-          imageUrl: Imagesurls.A8A0463,
-        );
-      },
     );
   }
 }
